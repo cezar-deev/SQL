3c1ac8a888f63663d0578a5549d62d157f823239
Cezar Cardoso <cezar.deev@gmail.com>
2022-02-11 14:01:33 -0300

    Atualizar

--- Rascunhos_Diarios/2-Fev_11-02-2022.sql
+++ Rascunhos_Diarios/2-Fev_11-02-2022.sql
@@ -12,3 +12,12 @@ status_junta
     FROM jntjunta
         WHERE execucao <> 0 and datacadastro='11.02.2022'
                ORDER BY datacadastro desc,cod_junta
+
+// -----------------------------------
+
+SELECT
+MDL.NOME_MODULO AS MODULO,
+DMT.COD_DM
+
+     FROM DMTDESENHOMONTAGEM DMT 
+     LEFT JOIN MDLMODULO MDL ON MDL.BOLD_ID = DMT.PMDLMODULO
